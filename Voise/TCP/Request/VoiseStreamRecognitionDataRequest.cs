d7da0cc306315c02fc76a237b4c5233e51ecd135
LBVDC\CirilloR <cirillor@lbv.org.br>
2018-09-12 13:07:47 -0300

    Melhorar documentação

--- Voise/TCP/Request/VoiseStreamRecognitionDataRequest.cs
+++ Voise/TCP/Request/VoiseStreamRecognitionDataRequest.cs
@@ -3,7 +3,7 @@
     internal class VoiseStreamRecognitionDataRequest
     {
 #pragma warning disable 0649
-        public string data;
+        public string data; // Raw audio bytes (base64 encoded)
 #pragma warning restore 0649
     }
 }
